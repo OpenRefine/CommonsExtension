dbffc78703b2e641d05426a104b65277352d03c9
Joey Salazar <jgsal@yahoo.com>
2022-10-20 05:24:32 -0600

    Reconcile filenames in project creation
    Fixes #56

--- src/test/java/org/openrefine/extensions/commons/importer/FileRecordToRowsTest.java
+++ src/test/java/org/openrefine/extensions/commons/importer/FileRecordToRowsTest.java
@@ -28,10 +28,10 @@ public class FileRecordToRowsTest {
         rows.add(frtr.getNextRowOfCells());
         rows.add(frtr.getNextRowOfCells());
 
-        Assert.assertEquals(rows.get(0), Arrays.asList("File:LasTres.jpg", "M127722", "Category:Costa Rica"));
+        Assert.assertEquals(rows.get(0).toString(), Arrays.asList("File:LasTres.jpg, M127722, Category:Costa Rica").toString());
         Assert.assertEquals(rows.get(1), Arrays.asList(null, null, "Category:Cute dogs"));
         Assert.assertEquals(rows.get(2), Arrays.asList(null, null, "Category:Costa Rican dogs"));
-        Assert.assertEquals(rows.get(3), Arrays.asList("File:Playa Gandoca.jpg", "M112933", null));
+        Assert.assertEquals(rows.get(3).toString(), Arrays.asList("File:Playa Gandoca.jpg", "M112933", null).toString());
         Assert.assertEquals(rows.get(4), null);
 
     }
