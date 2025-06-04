ba0abae333952f34752f19800354d4f19f37814b
Abbe98 <albin.post@gmail.com>
2025-06-04 15:41:48 +0200

    initiate I18N using core extension point

--- module/scripts/index/commons-importing-controller.js
+++ module/scripts/index/commons-importing-controller.js
@@ -1,22 +1,4 @@
-//Internationalization init
-var lang = navigator.language.split("-")[0]
-		|| navigator.userLanguage.split("-")[0];
-var dictionary = "";
-$.ajax({
-	url : "command/core/load-language?",
-	type : "POST",
-	async : false,
-	data : {
-	  module : "commons",
-//		lang : lang
-	},
-	success : function(data) {
-		dictionary = data['dictionary'];
-                lang = data['lang'];
-	}
-});
-$.i18n().load(dictionary, lang);
-// End internationalization
+I18NUtil.init("commons");
 
 Refine.CommonsImportingController = function(createProjectUI) {
   this._createProjectUI = createProjectUI;
